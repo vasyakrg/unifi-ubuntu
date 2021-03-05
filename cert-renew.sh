58cd7d7c48eba5801cc14de93e9e7aa9aaeeb51b
Vassiliy Yegorov <vasyakrg@gmail.com>
2021-03-05 10:05:58 +0700

    fix

--- cert-renew.sh
+++ cert-renew.sh
@@ -6,9 +6,9 @@ ca=/etc/letsencrypt/live/${DOMAIN}/chain.pem
 crt=/etc/letsencrypt/live/${DOMAIN}/cert.pem
 key=/etc/letsencrypt/live/${DOMAIN}/privkey.pem
 
-openssl pkcs12 -export -in $crt -inkey $key -out cloudkey.p12 -name unifi -CAfile $ca -caname root -password pass:aircontrolenterprise
-
-keytool -importkeystore -noprompt -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /usr/lib/unifi/data/keystore -srckeystore cloudkey.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi &>/dev/null
+openssl pkcs12 -export -in $crt -inkey $key -out cloudkey.p12 -name unifi -CAfile $ca -caname root -password pass:aircontrolenterprise && \
+keytool -importkeystore -noprompt -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /usr/lib/unifi/data/keystore \
+-srckeystore cloudkey.p12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi &>/dev/null
 
 service unifi stop
 service unifi start
